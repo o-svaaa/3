d1d36d9d5387fca1cbb1558cc65199a74778695c
o-svaaa <olesya.somova06@gmail.com>
2026-05-03 14:04:59 +0300

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 // test_connection.php
 try {
     $pdo = new PDO(
-        "mysql:host=localhost;dbname=82671;charset=utf8mb4",
+        "mysql:host=localhost;dbname=82814;charset=utf8mb4",
         "u82814",
         "3096918"
     );
